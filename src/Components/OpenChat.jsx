e2781837191e4d33e7ecb1d6f430d3cdbb088ea4
Jeyadeepak <83738609+JeyadeepakUR@users.noreply.github.com>
2025-02-18 09:21:28 +0530

    Update OpenChat.jsx

--- src/Components/OpenChat.jsx
+++ src/Components/OpenChat.jsx
@@ -36,8 +36,8 @@ const OpenChat = ({ Chats, User, socket }) => {
     const roomID = getUrlParams().get('roomID') || randomID(5);
     const generateCallUrl = async () => {
       if (!callUrlGenerated) { // Check if URL has already been generated
-        const appID = 1769300941;
-        const serverSecret = "b3c6f4cbfb8c34f44b3446b49971b445";
+        const appID = 756401678;
+        const serverSecret = "d02c2c9ff860f9a7de482335e058a650";
         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, randomID(5), randomID(5));
       
         const zp = ZegoUIKitPrebuilt.create(kitToken);
