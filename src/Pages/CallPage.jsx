54107f66bd5087e8fb0bdc16e15f8e14c66a6be9
Jeyadeepak <83738609+JeyadeepakUR@users.noreply.github.com>
2025-04-21 00:09:53 +0530

    arbaaz api

--- src/Pages/CallPage.jsx
+++ src/Pages/CallPage.jsx
@@ -76,8 +76,8 @@ export default function CallPage({ Chats }) {
   const navigate = useNavigate();
 
   const startCall = (element) => {
-    const appID = 1769300941;
-    const serverSecret = 'b3c6f4cbfb8c34f44b3446b49971b445';
+    const appID = 1719131048;
+    const serverSecret = 'ce5f456383d8d9bbc41405248a15acee';
     const userID = randomID(5);
     const userName = randomID(5);
     const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, userID, userName);
